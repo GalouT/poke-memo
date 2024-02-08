b56e3207db8c1715b108b358d3beb09282797e85
Galo <admin@admins-MBP.fibertel.com.ar>
2024-02-08 10:58:12 -0300

    se creo un contador de segundos

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,18 @@ import { WinnerModal } from "./componentes/WinnerModal";
 import { IMAGES } from "./import";
 import imageTitle from "/src/images/6543d0e84fd8dd7b77f40a5e907f7581.png";
 
-/* const IMAGES = [
-  "src/images/pokemons/001.png",
-  "src/images/pokemons/004.png",
-  "src/images/pokemons/007.png",
-  "src/images/pokemons/025.png",
-  "src/images/pokemons/052.png",
-  "src/images/pokemons/054.png",
-  "src/images/pokemons/132.png",
-  "src/images/pokemons/050.png",
-  "src/images/pokemons/066.png",
-  "src/images/pokemons/143.png",
-  "src/images/pokemons/095.png",
-  "src/images/pokemons/063.png",
-]
-  .flatMap((image) => [`a|${image}`, `b|${image}`])
-  .sort(() => Math.random() - 0.5); */
-
 function App() {
   const [guessed, setGuessed] = useState([]);
   const [selected, setSelected] = useState([]);
 
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+
   useEffect(() => {
+    if (selected.length === 1) {
+      setTimerRunning(true);
+    }
+
     if (selected.length === 2) {
       if (
         selected[0] !== selected[1] &&
@@ -42,10 +32,31 @@ function App() {
     }
   }, [selected]);
 
+  useEffect(() => {
+    let interval;
+
+    if (timerRunning) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerRunning]);
+
+  useEffect(() => {
+    if (guessed.length === IMAGES.length) {
+      setTimerRunning(false);
+    }
+  }, [guessed]);
+
   const handleReset = () => {
     location.reload();
     setGuessed([]);
     setSelected([]);
+
+    setTimerRunning(false);
+    setSeconds(0);
   };
 
   return (
@@ -57,7 +68,11 @@ function App() {
         {IMAGES.map((image, index) => {
           const [, url] = image.split("|");
           return guessed.length === IMAGES.length ? (
-            <WinnerModal key={index} handleReset={handleReset} />
+            <WinnerModal
+              key={index}
+              handleReset={handleReset}
+              seconds={seconds}
+            />
           ) : (
             <Card
               key={index}
@@ -74,6 +89,9 @@ function App() {
       <button className="reset-button" onClick={handleReset}>
         Reiniciar
       </button>
+      <div className="timer-container">
+        <span>Tiempo: {seconds} segundos</span>
+      </div>
     </div>
   );
 }
--- src/componentes/WinnerModal.jsx
+++ src/componentes/WinnerModal.jsx
@@ -1,8 +1,9 @@
-export function WinnerModal({ handleReset }) {
+export function WinnerModal({ handleReset, seconds }) {
   return (
     <section className="winner">
       <div className="text">
         <h2>Ganaste !</h2>
+        <p>Tardaste {seconds} segundos</p>
         <button className="reset-button" onClick={handleReset}>
           Empezar de nuevo
         </button>
